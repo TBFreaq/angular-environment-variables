003005b245ea9e622e1a2a4b3350c79702fee224
Yannick Haas <haas@medialesson.de>
2024-01-24 09:55:26 +0100

    Add code for runtime-host

--- apps/runtime-host/src/main.ts
+++ apps/runtime-host/src/main.ts
@@ -1,14 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { AppConfiguration } from '@angular-environment-variables/app-configuration';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+const config: AppConfiguration = {
+  angularEnvironment: process.env['ANGULAR_ENVIRONMENT'] === 'development' ? 'development' : 'production',
+  apiUrl: process.env['API_URL'] ?? null,
+  backgroundColor: process.env['BACKGROUND_COLOR'] ?? null
+}
+
+// If not found, doesn't throw an error.
+app.use(express.static(`${__dirname}/client/`, {}));
+
+// Provide configuration on /config
+app.get('/config', (_request: Request, res: Response) => {
+  res.json(config);
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+// Other requests should be redirected to our Angular client
+app.use('*', (_req: Request, res: Response) => {
+  res.sendFile(`${__dirname}/client/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
 });
